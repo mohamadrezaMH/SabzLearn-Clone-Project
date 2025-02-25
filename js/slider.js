bb36986a0a70c4594ee3b97f18dc4eeb1b5f19bc
mohamadrezaMH <mohamadreza.mh1783@gmail.com>
2025-02-25 17:17:22 +0330

    create course details page

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,31 @@
-const swiper = new Swiper('.swiper-container', {
+const swiper1 = new Swiper('.swiper-container-1', {
     speed: 800,
     loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+    breakpoints: {
+        576: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1200: {
+            slidesPerView: 3
+        }
+    }
+})
+
+const swiper2 = new Swiper('.swiper-container-2', {
+    speed: 800,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
 
     breakpoints: {
         576: {
